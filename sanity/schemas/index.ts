ebb11d384624d273c2fcc4803dc64ff9d2a37875
ell-ska <ella@skagerstrom.se>
2023-07-14 16:47:26 +0200

    make homepage a singleton document

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -6,17 +6,14 @@ import { character } from './sections/character'
 import { newsletter } from './sections/newsletter'
 import { callToAction } from './sections/callToAction'
 import { button } from './components/button'
-import type { SchemaTypeDefinition } from 'sanity'
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [
-		homepage,
-		hero,
-		trailer,
-		description,
-		character,
-		newsletter,
-		callToAction,
-		button,
-	],
-}
+export const schema = [
+	homepage,
+	hero,
+	trailer,
+	description,
+	character,
+	newsletter,
+	callToAction,
+	button,
+]
