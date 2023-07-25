2012a06353535ac3953ef1e84e24215369b0867c
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-07-25 09:45:51 +0200

    add trailer section, react-youtube npm package

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Trailer from '@/components/Trailer'
 import demoPicture from '@/public/picture.jpg'
 
 const Home = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center w-full'>
+    <main className='flex min-h-screen flex-col items-center w-full bg-darkColor'>
       <Header />
       <Hero backgroundImage={demoPicture} />
+      <Trailer />
     </main>
   )
 }
---
+++ components/Trailer.tsx
@@ -0,0 +1,37 @@
+'use client'
+import React from 'react'
+import YouTube, { YouTubeProps } from 'react-youtube'
+
+const Trailer = () => {
+  const onPlayerReady: YouTubeProps['onReady'] = event => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo()
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  }
+
+  return (
+    <>
+      <h3 className='text-lightColor p-12 md:p-24 md:text-lg text-center'>
+        Trapped in a time loop you must defy gravity, time, and the invading
+        alien race as you combat waves of enemies using aerial acrobatics and
+        bullets.
+      </h3>
+      <YouTube
+        videoId='FncHhkiWjDA'
+        opts={opts}
+        onReady={onPlayerReady}
+        className='flex flex-col justify-center w-screen md:px-24'
+      />
+    </>
+  )
+}
+
+export default Trailer
