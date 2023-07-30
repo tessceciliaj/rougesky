310a1a83265863a41cdc025ec3b4076857c58b82
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-07-30 17:35:46 +0200

    merge main into feature/emailinput

---
+++ components/EmailInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmailInput = () => {
+  return (
+    <section className='sectionTitle'>We need a backend to collect the emails?</section>
+  )
+}
+
+export default EmailInput
