dfff9812c98416680df121536fa00a9261349108
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-08-13 19:10:53 +0200

    removed enkelfjutt from promotion title

--- components/Promotion.tsx
+++ components/Promotion.tsx
@@ -5,7 +5,7 @@ const Promotion = () => {
   return (
     <section className='bg-black w-full min-h-screen flex flex-col justify-center items-center'>
       <h2 className='text-base md:text-xl font-bold text-lightColor text-center pb-2 px-6 md:px-24'>
-        Don't Miss Your Flight! Your Ticket to Adventure
+        Dont Miss Your Flight! Your Ticket to Adventure
       </h2>
       <ArrowDown className='text-lightColor m-2 text-xl animate-bounce' />
       <button className='mainBtn rotateBtn bg-accentColor'>
