eebb4dfd5a40a09dd43a96582d8af4cb79200289
ell-ska <ella@skagerstrom.se>
2024-05-09 21:58:20 +0200

    feat: get hero background image from sanity

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
-import Logo from './Logo'
-import { Footer as Props } from '@/sanity/types/homepage'
+import Image from 'next/image'
+import { urlForImage } from '@/sanity/lib/image'
+import { Footer as FooterProps } from '@/sanity/types/homepage'
 
-const Footer = ({ content }: { content: Props }) => {
+const Footer = ({ content }: { content: FooterProps }) => {
   return (
     <footer className='w-full text-lightColor flex flex-col gap-6 items-center pt-8 pb-6'>
       <Link href={content.link.link}>{content.link.text}</Link>
-      <Logo />
+      <Image
+        src={urlForImage(content.logo).url()}
+        alt='company logo'
+        width={100}
+        height={48}
+        className='max-h-12'
+      />
       <Link
         href={content.copyright.link}
         className='text-sm text-lightOpacity pb-4 px-4 hover:underline'>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,52 +1,27 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import { urlForImage } from '@/sanity/lib/image'
-import type { StaticImageData } from 'next/image'
 import type { Hero as HeroProps } from '@/sanity/types/homepage'
 
-const Hero = ({
-  content,
-  backgroundImage,
-}: {
-  content: HeroProps
-  backgroundImage: StaticImageData
-}) => {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage.src})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }
-
+const Hero = ({ content }: { content: HeroProps }) => {
   return (
-    <>
-      <div
-        className='flex flex-col h-screen items-center justify-center w-full px-4'
-        style={backgroundStyle}>
-        <div className='flex flex-col items-center justify-between h-screen pb-8 pt-[100px]'>
-          <Image
-            src={urlForImage(content.logo).url()}
-            alt='company logo'
-            width={350}
-            height={350}
-            className='py-4'
-          />
-          <div className='flex gap-2 justify-center'>
-            {content.ctas.map((button, index) => {
-              const color = index % 2 === 0 ? 'bg-darkColor' : 'bg-[#00D870]'
+    <div
+      className='flex h-screen items-end justify-center gap-2 pb-8 w-full px-4 bg-cover bg-center'
+      style={{
+        backgroundImage: `url(${urlForImage(content.background_image).url()})`,
+      }}>
+      {content.ctas.map((button, index) => {
+        const color = index % 2 === 0 ? 'bg-darkColor' : 'bg-[#00D870]'
 
-              return (
-                <Link
-                  href={button.link}
-                  key={button._key}
-                  className={'mainBtn ' + color}>
-                  {button.text}
-                </Link>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    </>
+        return (
+          <Link
+            href={button.link}
+            key={button._key}
+            className={'mainBtn ' + color}>
+            {button.text}
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
