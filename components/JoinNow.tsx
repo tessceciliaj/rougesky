7c9e31ed73b3cc8a5369ecc5815959ce51740b7c
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2024-05-10 18:25:45 +0200

    fix: join now div centered

--- components/JoinNow.tsx
+++ components/JoinNow.tsx
@@ -3,11 +3,13 @@ import { PortableText } from '@portabletext/react'
 
 import type { Newsletter as NewsletterProps } from '@/sanity/types/homepage'
 
-export const EmailInput = ({ content }: { content: NewsletterProps }) => {
+export const JoinNow = ({ content }: { content: NewsletterProps }) => {
   return (
     <section className='w-full py-20 bg-[#00417D] mx-auto'>
-      <div className='max-w-6xl flex flex-col items-center gap-6'>
-        <h2 className='font-title text-white text-6xl'>{content.title}</h2>
+      <div className='flex flex-col items-center gap-6 p-6'>
+        <h2 className='font-title text-white text-6xl text-center'>
+          {content.title}
+        </h2>
         <div className='text-lightColor text-center max-w-xl'>
           <PortableText value={content.desc} />
         </div>
