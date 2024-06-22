9ff072dbfb678f680fdd7e97816affb085229585
Ella <96110484+ell-ska@users.noreply.github.com>
2024-06-22 14:01:39 +0200

    fix: description image aspect ratio (#53)

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,34 +1,39 @@
 import Image from 'next/image'
 import { PortableText } from '@portabletext/react'
-import { urlForImage } from '@/sanity/lib/image'
+
+import { getImageDimensions, urlForImage } from '@/sanity/lib/image'
 
 import type { Description as DescriptionProps } from '@/sanity/schemas/sections/description'
 
 export const Description = ({ content }: { content: DescriptionProps }) => {
   return (
-    <div className='flex max-w-6xl flex-col px-8 text-center'>
-      <h2 className='text-primary px-6 pb-6 pt-12 text-center font-title text-2xl font-bold md:px-24 md:pb-12 md:text-4xl'>
+    <div className='flex max-w-6xl flex-col px-8 pb-16 text-center'>
+      <h2 className='px-6 pb-6 pt-12 text-center font-title text-2xl font-bold text-primary md:px-24 md:pb-12 md:text-4xl'>
         {content.title}
       </h2>
-      <h3 className='text-light pb-16 md:text-lg'>{content.subtitle}</h3>
-      <div className='text-light flex w-full flex-wrap justify-center gap-4'>
-        {content.descriptions.map((card) => (
-          <div key={card._key} className='min-w-[12rem] max-w-xs flex-1'>
-            <Image
-              src={urlForImage(card.image).url()}
-              width={350}
-              height={200}
-              alt={card.image.alt || ''}
-              className='h-[300px] rounded object-cover'
-            />
-            <h4 className='text-primary pt-8 font-title text-4xl'>
-              {card.title}
-            </h4>
-            <div className='pt-4'>
-              <PortableText value={card.text} />
+      <h3 className='pb-16 text-light md:text-lg'>{content.subtitle}</h3>
+      <div className='flex w-full flex-wrap justify-center gap-x-8 gap-y-12 text-light'>
+        {content.descriptions.map((card) => {
+          const dimensions = getImageDimensions(card.image)
+
+          return (
+            <div key={card._key} className='min-w-[12rem] max-w-xs flex-1'>
+              <Image
+                src={urlForImage(card.image).url()}
+                width={dimensions.width}
+                height={dimensions.height}
+                alt={card.image.alt || ''}
+                className='border-2 border-primary'
+              />
+              <h4 className='pt-8 font-title text-4xl text-primary'>
+                {card.title}
+              </h4>
+              <div className='pt-4'>
+                <PortableText value={card.text} />
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   )
